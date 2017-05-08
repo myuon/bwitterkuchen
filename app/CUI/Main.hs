aa581532c152c31543f2e3f710eb7ce2754dda75
myuon <ioi.joi.koi.loi@gmail.com>
2017-05-08 22:00:10 +0900

    CUI

--- app/CUI/Main.hs
+++ app/CUI/Main.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import Control.Lens
+import Data.Aeson (FromJSON)
 import Web.Twitter.Conduit
 import Web.Twitter.Types.Lens
 import Control.Monad.Reader
@@ -29,18 +30,31 @@ type AuthM = ReaderT TWInfo IO
 runAuth :: AuthM a -> IO a
 runAuth m = genTWInfo >>= runReaderT m
 
+callM :: (FromJSON res, ?mgr :: Manager) => APIRequest api res -> AuthM res
+callM api = ask >>= \twInfo -> lift $ call twInfo ?mgr api
+
 fetch :: (?mgr :: Manager) => Integer -> AuthM [Status]
-fetch n = do
-  twInfo <- ask
-  lift $ call twInfo ?mgr $ homeTimeline & count ?~ n
+fetch n = callM $ homeTimeline & count ?~ n
+
+favo :: (?mgr :: Manager) => StatusId -> AuthM Status
+favo st = callM $ favoritesCreate st
+
+tweet :: (?mgr :: Manager) => String -> AuthM Status
+tweet txt = callM $ update $ T.pack txt
+
+replyTo :: (?mgr :: Manager) => StatusId -> String -> AuthM Status
+replyTo st txt = callM $ update (T.pack txt) & inReplyToStatusId ?~ st
 
 main = do
   mgr <- newManager tlsManagerSettings
   let ?mgr = mgr
-  xs <- getArgs
+  let showStatus s = T.unpack (s^.text)
 
-  case xs of
-    ("fetch":[]) -> mapM_ (putStrLn . T.unpack) . fmap (\s -> s^.text) =<< runAuth (fetch 20)
-    ("fetch":n:_) -> mapM_ (putStrLn . T.unpack) . fmap (\s -> s^.text) =<< runAuth (fetch (read n))
+  getArgs >>= \xs -> case xs of
+    ("fetch":[]) -> mapM_ putStrLn . fmap showStatus =<< runAuth (fetch 20)
+    ("fetch":n:_) -> mapM_ putStrLn . fmap showStatus =<< runAuth (fetch (read n))
+    ("favo":n:_) -> putStrLn . showStatus =<< runAuth (favo (read n))
+    ("tweet":txt:_) -> putStrLn . showStatus =<< runAuth (tweet txt)
+    ("replyTo":n:txt:_) -> putStrLn . showStatus =<< runAuth (replyTo (read n) txt)
     _ -> print xs
 
